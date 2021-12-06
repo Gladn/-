3aefae211d321c10438c6b38ae4c57c56a8b7312
Alexander Gladkov <92585647+Gladn@users.noreply.github.com>
2021-12-07 02:41:05 +0700

    Add files via upload

---
+++ 1-2 Class+Interface C# WForms/lab1/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace Class_interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 46);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(274, 414);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(567, 410);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Вывести в файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(564, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 60);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Массив с полями разных типов\r\nСортировка по разным полям\r\nЗаписать в файл";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(567, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 45);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Сортировка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(804, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(303, 46);
+            this.dataGridView2.MultiSelect = false;
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 29;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(255, 414);
+            this.dataGridView2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(564, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 60);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Нажмите на клиента, \r\nчтобы увидеть его товары\r\n Выбран:\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(567, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(567, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(827, 46);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 29;
+            this.dataGridView3.Size = new System.Drawing.Size(300, 409);
+            this.dataGridView3.TabIndex = 10;
+            this.dataGridView3.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(567, 352);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 52);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Сортировать прибль";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 463);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ 1-2 Class+Interface C# WForms/lab1/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using System.Collections;
+
+namespace Class_interface
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Customer> list_customer = new List<Customer>()
+        {new Customer(1, "Алексей Алексеев"),
+        new Customer(2, "Мария Андрианова"),
+        new Customer(3, "Анастасия Платова"),
+        new Customer(4, "Александра Анисимова"),
+        new Customer(5, "Милена Белова"),
+        new Customer(6, "Тимур Бирюков"),
+        new Customer(7, "Макар Иванов"),
+        new Customer(8, "Виктория Кузьмина"),
+        new Customer(9, "Максим Тарасов"),
+        new Customer(10, "Даниил Яковлев"),
+        new Customer(11, "Кира Галкина"),
+        new Customer(12, "Ева Крылова")};
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = list_customer;
+            setdatagridview1columnwidth();
+            label3.Visible = false;
+            label4.Visible = false;
+            dataGridView3.DataSource = 1;
+            dataGridView3.Update();
+
+        }
+        private void setdatagridview1columnwidth()
+        {
+            dataGridView1.Columns[0].Width = 60;
+            dataGridView1.Columns[1].Width = 200;
+        }
+        private void setdatagridview2columnwidth()
+        {
+            if (dataGridView2.Rows.Count != 0)
+            {
+                dataGridView2.Columns[0].Width = 60;
+                dataGridView2.Columns[1].Width = 120;
+                dataGridView2.Columns[2].Width = 60;
+                dataGridView2.Columns[3].Visible = false;
+                dataGridView2.Columns[4].Visible = false;
+            }
+        }
+        public class Customer
+        {
+            [System.ComponentModel.DisplayName("Ключи")]
+            public int ID_customer { get; set; }
+
+            [System.ComponentModel.DisplayName("Имя Фамилия")]
+            public string Name_customer { get; set; }
+            public Customer() { }
+            public Customer(int id_customer, string name_customer)
+            {
+                Name_customer = name_customer;
+                ID_customer = id_customer;
+            }
+
+        }
+        class Product : Customer
+        {
+            [System.ComponentModel.DisplayName("Ключи")]
+            public int ID_product { get; set; }
+
+            [System.ComponentModel.DisplayName("Товары")]
+            public string Name_product { get; set; }
+
+            [System.ComponentModel.DisplayName("Цены")]
+            public int Price_product { get; set; }
+            Customer inner;
+            public Product(int id_customer, string name_customer, int id_product, string name_product, int price) : base(id_customer, name_customer)
+            {
+                inner = new Customer(id_customer, name_customer);
+                ID_product = id_product; Name_product = name_product; Price_product = price;
+            }
+            public string getSecond(string name_customer)
+            {
+                return name_customer;
+            }
+            public int getSum(int price)
+            {
+                return 0;
+            }
+        }
+
+        List<Product> list_product = new List<Product>(){
+        new Product(1,"Алексей Алексеев", 1, "Ноутбук", 10000),
+        new Product(1,"Алексей Алексеев", 2, "Провода №1", 500),
+        new Product(1,"Алексей Алексеев", 3, "Провода №2", 1000),
+        new Product(2,"Мария Андрианова", 1, "Телефон", 5000),
+        new Product(2,"Мария Андрианова", 1, "Ноутбук", 10000),
+        new Product(3,"Анастасия Платова", 3, "Провода №2", 1000),
+        new Product(4,"Александра Анисимова", 6, "Консоль", 2000),
+        new Product(5,"Милена Белова", 4, "Радио", 500),
+        new Product(6,"Тимур Бирюков", 5, "Видеокарта", 4000)};
+
+
+
+        public void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            label3.Visible = true;
+            label4.Visible = true;
+            string s = dataGridView1.CurrentCell.Value.ToString();
+            label3.Text = s;
+            ArrayList list = new ArrayList();
+            for (int i = 0; i < list_product.Count; i++)
+            {
+                if (s == list_product[i].Name_customer)
+                {
+                    list.Add(list_product[i]);
+                }
+            }
+            dataGridView2.DataSource = list;
+            dataGridView2.Update();
+            setdatagridview2columnwidth();
+
+
+            int sum = 0;
+            for (int i = 0; i < list_product.Count; i++)
+            {
+                if (s == list_product[i].Name_customer)
+                {
+                    sum += list_product[i].Price_product;
+                }
+
+            }
+            label4.Text = sum.ToString();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application(); // класс для запуска Excel
+            ExcelApp.Application.Workbooks.Add(Type.Missing);
+            ExcelApp.Columns.ColumnWidth = 25;
+            ExcelApp.Cells[1, 1] = "Прибль";
+            ExcelApp.Cells[1, 2] = "Ключ";
+            ExcelApp.Cells[1, 3] = "Клиент";
+
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.Rows.Count; j++)
+                {
+                    ExcelApp.Cells[j + 2, i + 1] = dataGridView1[i, j].Value;
+                }
+            }
+
+            ExcelApp.Visible = true;
+            ExcelApp.UserControl = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        
+        
+            for (int i = 0; i < list_product.Count; i++)
+            {
+                int sum1 = 0;
+                for (int j = i+1; j < list_product.Count; j++)
+                {
+                    if (list_product[i].Name_customer == list_product[j].Name_customer)
+                    {
+                        sum1 = list_product[i].Price_product;                       
+                        list_product[i].Price_product = sum1 + list_product[j].Price_product;
+                       
+                    }                    
+                }
+            }
+
+            int index = list_product.Count - 1;
+            while (index > 0) {
+                if (list_product[index].Name_customer == list_product[index - 1].Name_customer) 
+                {
+                    list_product.RemoveAt(index);
+                    index--; }
+                else index--;
+            }
+
+            dataGridView3.DataSource = list_product;
+
+
+            for (int i = 0; i < list_product.Count; i++)
+            {
+                int j_max = i;
+                for (int j = i + 1; j < list_product.Count; j++)
+                {
+                    if (list_product[j_max].Price_product < list_product[j].Price_product)
+                    {
+                        j_max = j;
+                    }
+                }
+                Product temp = list_product[i];
+                list_product[i] = list_product[j_max];
+                list_product[j_max] = temp;
+            }
+            dataGridView1.DataSource = list_product;
+            dataGridView2.Visible = false;
+            dataGridView1.Width = 500;
+            dataGridView1.Columns.RemoveAt(0);
+            dataGridView1.Columns.RemoveAt(0);
+            dataGridView1.Update();
+        }
+    }
+}
+
+
