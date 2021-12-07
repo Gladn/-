ab2f8a73e3918dc5b257fddcc2ab15ce30abca67
Alexander Gladkov <92585647+Gladn@users.noreply.github.com>
2021-12-07 21:47:13 +0700

    Update Source.cpp

--- 3. polymorphism C++/3. polymorphism/Source.cpp
+++ 3. polymorphism C++/3. polymorphism/Source.cpp
@@ -1,6 +1,5 @@
-//Комплексные числа 
-//https://ru.wikipedia.org/wiki/%D0%9A%D0%BE%D0%BC%D0%BF%D0%BB%D0%B5%D0%BA%D1%81%D0%BD%D0%BE%D0%B5_%D1%87%D0%B8%D1%81%D0%BB%D0%BE
-//https://ru.wikipedia.org/wiki/%D0%9E%D0%BF%D0%B5%D1%80%D0%B0%D1%82%D0%BE%D1%80%D1%8B_%D0%B2_C_%D0%B8_C%2B%2B
+//ГЉГ®Г¬ГЇГ«ГҐГЄГ±Г­Г»ГҐ Г·ГЁГ±Г«Г  
+
 
 #include <stdio.h>
 #include <windows.h>
@@ -19,15 +18,15 @@ public: complex_numbers(float a = 0, float b = 0) {
 		this->b = b; 
 	} 
 	
-	complex_numbers operator + (complex_numbers&); //1. сложение комплексных чисел
+	complex_numbers operator + (complex_numbers&); //1. Г±Г«Г®Г¦ГҐГ­ГЁГҐ ГЄГ®Г¬ГЇГ«ГҐГЄГ±Г­Г»Гµ Г·ГЁГ±ГҐГ«
 
-	complex_numbers operator * (complex_numbers&); //2. умножение комплексных чисел
+	complex_numbers operator * (complex_numbers&); //2. ГіГ¬Г­Г®Г¦ГҐГ­ГЁГҐ ГЄГ®Г¬ГЇГ«ГҐГЄГ±Г­Г»Гµ Г·ГЁГ±ГҐГ«
 
-	complex_numbers& operator + (void); //3. смена знака мнимой части
+	complex_numbers& operator + (void); //3. Г±Г¬ГҐГ­Г  Г§Г­Г ГЄГ  Г¬Г­ГЁГ¬Г®Г© Г·Г Г±ГІГЁ
 
-	complex_numbers& operator ++ (void); //4. постфикс инкремент
+	complex_numbers& operator ++ (void); //4. ГЇГ®Г±ГІГґГЁГЄГ± ГЁГ­ГЄГ°ГҐГ¬ГҐГ­ГІ
 
-	complex_numbers operator ++ (int); //5. префикс инкремент
+	complex_numbers operator ++ (int); //5. ГЇГ°ГҐГґГЁГЄГ± ГЁГ­ГЄГ°ГҐГ¬ГҐГ­ГІ
 
 	void show(void); 
 };
@@ -56,33 +55,33 @@ complex_numbers complex_numbers::operator ++ (int) {
 }
 
 void complex_numbers::show(void) {
-	//лишний "-" перед мнимой частью
+	//Г«ГЁГёГ­ГЁГ© "-" ГЇГҐГ°ГҐГ¤ Г¬Г­ГЁГ¬Г®Г© Г·Г Г±ГІГјГѕ
 	if (b < 0) printf("\n%.2f%.2fi", a, b);
 	else printf("\n%.2f+%.2fi", a, b);
 }
 
 int main(void) { 
 	setlocale(LC_ALL, "Russian");
-	//сложение
+	//Г±Г«Г®Г¦ГҐГ­ГЁГҐ
 	complex_numbers com1(1, -1);
 	complex_numbers com2(2, -1);
 	complex_numbers com_sum = com1 + com2;
-	printf("\n\nПервое число"); com1.show();
-	printf("\nВторое число"); com2.show();
-	printf("\nСумма первого и второго"); com_sum.show();
-	//умножение
+	printf("\n\nГЏГҐГ°ГўГ®ГҐ Г·ГЁГ±Г«Г®"); com1.show();
+	printf("\nГ‚ГІГ®Г°Г®ГҐ Г·ГЁГ±Г«Г®"); com2.show();
+	printf("\nГ‘ГіГ¬Г¬Г  ГЇГҐГ°ГўГ®ГЈГ® ГЁ ГўГІГ®Г°Г®ГЈГ®"); com_sum.show();
+	//ГіГ¬Г­Г®Г¦ГҐГ­ГЁГҐ
 	complex_numbers com_mult = com1 * com2;
-	printf("\nПроизведение первого второго"); com_mult.show();
-	//знак мнимой части
+	printf("\nГЏГ°Г®ГЁГ§ГўГҐГ¤ГҐГ­ГЁГҐ ГЇГҐГ°ГўГ®ГЈГ® ГўГІГ®Г°Г®ГЈГ®"); com_mult.show();
+	//Г§Г­Г ГЄ Г¬Г­ГЁГ¬Г®Г© Г·Г Г±ГІГЁ
 	com1 = +com1;
-	printf("\nСмена знака мнимой части первого числа"); com1.show();
-	//постфикс
-	printf("\nВыполнение com3=com1++");
+	printf("\nГ‘Г¬ГҐГ­Г  Г§Г­Г ГЄГ  Г¬Г­ГЁГ¬Г®Г© Г·Г Г±ГІГЁ ГЇГҐГ°ГўГ®ГЈГ® Г·ГЁГ±Г«Г "); com1.show();
+	//ГЇГ®Г±ГІГґГЁГЄГ±
+	printf("\nГ‚Г»ГЇГ®Г«Г­ГҐГ­ГЁГҐ com3=com1++");
 	complex_numbers com3 = com1++;
 	printf("\ncom3="); com3.show();
 	printf("\ncom1="); com1.show();
-	//префикс
-	printf("\nВыполнение com4=++com2");
+	//ГЇГ°ГҐГґГЁГЄГ±
+	printf("\nГ‚Г»ГЇГ®Г«Г­ГҐГ­ГЁГҐ com4=++com2");
 	complex_numbers com4 = ++com2;
 	printf("\ncom4="); com4.show();
 	printf("\ncom2="); com2.show();
